4c09f483d6f2db74ae1130630d1ebe37c6a241a4
onobrain <dreemerups@gmail.com>
2024-04-23 14:57:14 +0100

    files added

---
+++ src/Content.js
@@ -0,0 +1,9 @@
+export const Content = () => {
+  return (
+    <div className='Content'>
+        < div >Hard work beats talent</div>
+        < br />
+        
+    </div>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="logo">
+          Logo
+          <div className="menu">
+            <div className="menu-item">About</div>
+            <div className="menu-item">Journal</div>
+            <div className="menu-item">Contact</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
